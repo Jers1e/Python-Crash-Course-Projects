502b9aa1a02b54020b948fd96b1432b7226a4f93
Jessica <jessicagthompson03@gmail.com>
2023-05-12 13:13:29 -0500

    Added car.py class program

--- Chapter_9_Classes/restaurant.py
+++ Chapter_9_Classes/restaurant.py
@@ -6,12 +6,23 @@ class Restaurant:
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
+        """Gives the name and cuisine type"""
         print(f"This restaurant is called {self.restaurant_name} and has {self.cuisine_type} cuisine.")
 
     def open_restaurant(self):
+        """Says that the restaurant is open"""
         print(f"The restaurant, {self.restaurant_name} is open!")
 
 
 my_restaurant = Restaurant('Kaze Sushi', 'Thai/Japanese')
 my_restaurant.describe_restaurant()
 my_restaurant.open_restaurant()
+
+your_restaurant = Restaurant('Tom and Jerry', 'Greek')
+your_restaurant.describe_restaurant()
+your_restaurant.open_restaurant()
+
+our_restaurant = Restaurant('Chicken Shack', 'Southern')
+our_restaurant.describe_restaurant()
+our_restaurant.open_restaurant()
+
